69317539f3ad8034a5a8862bdcdbb45706924905
karaage0703 <coldchicken@gmail.com>
2019-06-07 07:55:39 +0900

    Add script

---
+++ install-open-pose.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt-get update
+
+# Install library
+pip3 install --upgrade cython
+sudo apt-get -y install libopenblas-dev
+sudo apt-get -y install liblapacke-dev
+sudo apt-get -y install gfortran
+sudo apt-get -y install llvm-7*
+echo 'export PATH="/usr/lib/llvm-7/bin:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
+sudo apt-get -y install python3-scipy
+
+pip3 install tqdm
+pip3 install slidingwindow
+pip3 install pycocotools
+
+# Setup tf-pose-estimation
+cd && git clone https://github.com/ildoonet/tf-pose-estimation.git
+cd ~/tf-pose-estimation/models/graph/cmu
+bash download.sh
+cd ~/tf-pose-estimation/tf_pose/pafprocess
+sudo apt-get -y install swig
+swig -python -c++ pafprocess.i && python3 setup.py build_ext --inplace
+cd ~/tf-pose-estimation/
+
+# Reference site
+# https://toramamma.blogspot.com/2019/04/jetson-nano-tensorflowopenpose.html
