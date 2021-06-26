f221b1b03e19c8e2afff96bf3254c1e53ad7ad1c
Kazuhiro Ouchi <kanpapa@kanpapa.com>
2021-06-26 21:07:31 +0900

    鍵が変更になっていたので修正

--- install-ros-melodic.sh
+++ install-ros-melodic.sh
@@ -7,7 +7,8 @@ echo "[Add the ROS repository]"
 if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 fi
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+#sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 echo "[Update the package]"
 sudo apt-get update
